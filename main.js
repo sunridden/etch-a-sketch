94dfe621a1cd90f65e8646fdf6b3f46629ff0dc1
Kevin <kevinszeto9449@gmail.com>
2022-04-29 21:59:07 -0700

    Added event listener detecting clicks and drawing with specified color

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 function createGrid() {
-    const grid = document.querySelector('#grid');
+    const grid = document.getElementById('grid');
 
     //creates 16x16 square grid
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             const box = document.createElement('div');
+            box.classList.add('cell');
             //Check cases for left grid pieces and upper grid pieces
             if (i === 0 && j === 0) {
-                box.setAttribute('style', 'background-color: white; border-style: solid; border-width: 2px; height: 46px; width: 46px;');
+                box.setAttribute('style', 'border-style: solid; border-width: 2px; height: 46px; width: 46px;');
             } else if (i === 0) {
-                box.setAttribute('style', 'background-color: white; border-style: solid solid solid none; border-width: 2px; height: 46px; width: 48px;');
+                box.setAttribute('style', 'border-style: solid solid solid none; border-width: 2px; height: 46px; width: 48px;');
             } else if (j === 0) {
-                box.setAttribute('style', 'background-color: white; border-style: none solid solid solid; border-width: 2px; height: 48px; width: 46px;');
+                box.setAttribute('style', 'border-style: none solid solid solid; border-width: 2px; height: 48px; width: 46px;');
             } else {
-                box.setAttribute('style', 'background-color: white; border-style: none solid solid none; border-width: 2px; height: 48px; width: 48px;'); //normal pixel sets right and downward border
+                box.setAttribute('style', 'border-style: none solid solid none; border-width: 2px; height: 48px; width: 48px;'); //normal pixel sets right and downward border
             }
             grid.appendChild(box);
         }
     }
+    
+    draw();
  
+}
+
+function draw() {
+    const grid = document.getElementById('grid');
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mousedown', clickInput)
+    })
+
+    function clickInput() {
+        this.style.backgroundColor = 'green';
+    }
 }
 
 createGrid();
